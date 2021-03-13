58deda086e1389947ff1a919fde1ed3e869447d4
amoh-godwin <amohgyebigodwin@gmail.com>
2021-03-13 11:44:31 +0000

    connect the connector to the UI layer

--- main.py
+++ main.py
@@ -3,11 +3,14 @@ import sys
 from PyQt5.QtGui import QGuiApplication
 from PyQt5.QtQml import QQmlApplicationEngine
 
+from connector import Connector
+
 
 app = QGuiApplication(sys.argv)
 
 engine = QQmlApplicationEngine()
 engine.load('UI/main.qml')
+engine.rootObjects()[0].setProperty('connector', Connector())
 engine.quit.connect(app.quit)
 
 sys.exit(app.exec())
