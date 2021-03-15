ff8b764d6099e3c6df8d2b9c637828aeaa01036f
amoh-godwin <amohgyebigodwin@gmail.com>
2021-03-15 22:21:41 +0000

    add column

--- misc.py
+++ misc.py
@@ -13,7 +13,7 @@ def convert_users_to_json(data):
             row['country'], row['registered'], row['others'], \
             row['win_photos'], row['python_gui'], row['py_gui_ufb'], \
             row['pyqt'], row['toda'], row['soft_dev'], \
-            row['last_visited'] = x
+            row['last_visited'], row['pratical_project_py'] = x
         info.append(row)
 
     return info
