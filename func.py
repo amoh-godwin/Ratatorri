5474dc41d7294e08a1d84c18a419bb1dd205d5be
amoh-godwin <amohgyebigodwin@gmail.com>
2021-03-16 00:48:32 +0000

    rewrite func code

--- func.py
+++ func.py
@@ -31,25 +31,28 @@ def start():
 
 def nroll_cou(co, linker, signal_nroll, signal_status):
     u_nons = model.select_none_course(co)
-    json = convert_two_to_json(u_nons, 'email', 'passcode')
+    json = convert_two_to_json(u_nons, 'email', 'status')
     signal_nroll.emit(json)
 
-    return
     count = -1
     for x in u_nons:
         count += 1
         signal_status.emit(count, 'registering')
         print('log0')
         bow = loggin(x[0], x[1])
+        sleep(0.2)
         if bow:
             signal_status.emit(count, 'in progress')
             ret = nroll_other(bow, linker)
             if ret:
-                signal_status.emit(count, 'success')
-                ret_sta = model.add_users_course(x[0], co)
-                print(f'count {count}: {ret_sta}')
-                sleep(2)
-            print(ret, '\n\n')
-            sleep(5)
+                if model.add_users_course(x[0], co):
+                    signal_status.emit(count, 'success')
+                sleep(0.2)
+            else:
+                signal_status.emit(count, 'error')
+
+            sleep(1)
+        else:
+            signal_status.emit(count, 'unable to log')
         # close out
         bow.close()
